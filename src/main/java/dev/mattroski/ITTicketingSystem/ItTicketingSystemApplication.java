a44c3b9bb83e3046d7f59e189078ea9570df7cee
MattRoski <mattroski88@gmail.com>
2022-07-05 20:33:09 -0500

    first commit

---
+++ src/main/java/dev/mattroski/ITTicketingSystem/ItTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.mattroski.ITTicketingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItTicketingSystemApplication.class, args);
+	}
+
+}
